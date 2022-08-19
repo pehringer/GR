5029c7fb61994d059d6f56f48560e4d2ea3e31dc
pehringer <jacob.ray.pehringer@gmail.com>
2022-08-19 12:59:37 -0600

    Done with parsing line.

--- README.md
+++ README.md
@@ -8,40 +8,39 @@ Vaild G-code file syntax for the reader are defined below in Backus–Naur form.
 
 ###### Syntax Equivalent Characters
 ```
-<argument-char> ::= "A" | "B" | "C" | "D" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "P" | "Q" | "R" | "S" | "T" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "p" | "q" | "r" | "s" | "t" | "x" | "y" | "z" | "*"
-<digit-char> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-<newline-char> ::= "\n" | "\r"
-<text-char> ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "|" | "!" | "#" | "$" | "%" | "&" | "*" | "+" | "," | "-" | "." | "/" | ":" | ";" | ">" | "=" | "<" | "?" | "@" | "[" | "\" | "]" | "^" | "_" | "`" | "{" | "}" | "~" | " " | "\t"
-<whitespace-char> ::= " " | "\t"
+<argumentChar> ::= "A" | "B" | "C" | "D" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "P" | "Q" | "R" | "S" | "T" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "p" | "q" | "r" | "s" | "t" | "x" | "y" | "z" | "*"
+<digitChar> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+<newlineChar> ::= "\n" | "\r"
+<textChar> ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "|" | "!" | "#" | "$" | "%" | "&" | "*" | "+" | "," | "-" | "." | "/" | ":" | ";" | ">" | "=" | "<" | "?" | "@" | "[" | "\" | "]" | "^" | "_" | "`" | "{" | "}" | "~" | " " | "\t"
+<whitespaceChar> ::= " " | "\t"
 ```
 
 ###### Argument Syntax
 ```
-<sign> ::= "+" | "-" | ""
-<digits> ::= <digit-char> <digits> | <digit-char>
-<number> ::= <sign> <digits> | <sign> <digits> "." | <sign> <digits> "." <digits> | <sign> "." <digits> | ""
-<argument> ::= <argument-char> <number>
+<Sign> ::= "+" | "-" | ""
+<Digits> ::= <digitChar> <Digits> | <digitChar>
+<Number> ::= <Sign> <Digits> | <Sign> <Digits> "." | <Sign> <Digits> "." <Digits> | <Sign> "." <Digits> | ""
+<Argument> ::= <argumentChar> <Number>
 ```
 
 ###### Comment Syntax
 ```
-<text> ::= <text-char> <text> | ""
-<inline-comment> ::= "(" <text> ")"
-<ending-comment> ::= ";" <text> | ""
+<Text> ::= <textChar> <Text> | ""
+<InlineComment> ::= "(" <Text> ")"
+<EndingComment> ::= ";" <Text> | ""
 ```
 
 ###### Newline Syntax
 ```
-<newline> ::= <newline-char> <newline> | <newline-char>
+<Newline> ::= <newlineChar> <Newline> | <newlineChar>
 ```
 
 ###### Whitespace Syntax
 ```
-<whitespace> ::= <whitespace-char> <whitespace> | <whitespace-char>
+<Whitespace> ::= <whitespaceChar> <whitespace> | <whitespaceChar>
 ```
 
 ###### Line Syntax
 ```
-<contents> ::= <argument> <contents> | <inline-comment> <contents> | <whitespace> <contents> | ""
-<line> ::= <contents> <ending-comment> <newline>
+<Line> ::= <Argument> <Line> | <InlineComment> <Line> | <Whitespace> <Line> | <EndingComment> <Newline> | <Newline>
 ```
--- gCodeParser.c
+++ gCodeParser.c
@@ -1,175 +1,153 @@
 #include<stdio.h>
 
 
-/*
-  Lookup Tables And Related Constants:
-*/
+static enum CharType {
+  argumentChar   = 0x01,
+  digitChar      = 0x02,
+  newlineChar    = 0x04,
+  textChar       = 0x08,
+  whitespaceChar = 0x10,
+} Warning0;
 
 
-static const char ARGUMENT_CHAR   = 0x01;
-static const char DIGIT_CHAR      = 0x02;
-static const char NEWLINE_CHAR    = 0x04;
-static const char TEXT_CHAR       = 0x08;
-static const char WHITESPACE_CHAR = 0x10;
-static const char EQUIVALENT[] = {
-  0x00, //000 '\0'
-  0x00, //001
-  0x00, //002
-  0x00, //003
-  0x00, //004
-  0x00, //005
-  0x00, //006
-  0x00, //007
-  0x00, //008
-  0x18, //009 '\t'
-  0x04, //010 '\n'
-  0x00, //011
-  0x00, //012
-  0x04, //013 '\r'
-  0x00, //014
-  0x00, //015
-  0x00, //016
-  0x00, //017
-  0x00, //018
-  0x00, //019
-  0x00, //020
-  0x00, //021
-  0x00, //022
-  0x00, //023
-  0x00, //024
-  0x00, //025
-  0x00, //026
-  0x00, //027
-  0x00, //028
-  0x00, //029
-  0x00, //030
-  0x00, //031
-  0x18, //032 ' '
-  0x08, //033 '!'
-  0x00, //034 '"'
-  0x08, //035 '#'
-  0x08, //036 '$'
-  0x08, //037 '%'
-  0x08, //038 '&'
-  0x00, //039 '''
-  0x00, //040 '('
-  0x00, //041 ')'
-  0x09, //042 '*'
-  0x08, //043 '+'
-  0x08, //044 ','
-  0x08, //045 '-'
-  0x08, //046 '.'
-  0x08, //047 '/'
-  0x0A, //048 '0'
-  0x0A, //049 '1'
-  0x0A, //050 '2'
-  0x0A, //051 '3'
-  0x0A, //052 '4'
-  0x0A, //053 '5'
-  0x0A, //054 '6'
-  0x0A, //055 '7'
-  0x0A, //056 '8'
-  0x0A, //057 '9'
-  0x08, //058 ':'
-  0x08, //059 ';'
-  0x08, //060 '<'
-  0x08, //061 '='
-  0x08, //062 '>'
-  0x08, //063 '?'
-  0x08, //064 '@'
-  0x09, //065 'A'
-  0x09, //066 'B'
-  0x09, //067 'C'
-  0x09, //068 'D'
-  0x09, //069 'E'
-  0x09, //070 'F'
-  0x09, //071 'G'
-  0x09, //072 'H'
-  0x09, //073 'I'
-  0x09, //074 'J'
-  0x09, //075 'K'
-  0x09, //076 'L'
-  0x09, //077 'M'
-  0x09, //078 'N'
-  0x08, //079 'O'
-  0x09, //080 'P'
-  0x09, //081 'Q'
-  0x09, //082 'R'
-  0x09, //083 'S'
-  0x09, //084 'T'
-  0x08, //085 'U'
-  0x08, //086 'V'
-  0x08, //087 'W'
-  0x09, //088 'X'
-  0x09, //089 'Y'
-  0x09, //090 'Z'
-  0x08, //091 '['
-  0x08, //092 '\'
-  0x08, //093 ']'
-  0x08, //094 '^'
-  0x08, //095 '_'
-  0x08, //096 '`'
-  0x09, //097 'a'
-  0x09, //098 'b'
-  0x09, //099 'c'
-  0x09, //100 'd'
-  0x08, //101 'e'
-  0x09, //102 'f'
-  0x09, //103 'g'
-  0x09, //104 'h'
-  0x09, //105 'i'
-  0x09, //106 'j'
-  0x09, //107 'k'
-  0x09, //108 'l'
-  0x09, //109 'm'
-  0x09, //110 'n'
-  0x08, //111 'o'
-  0x09, //112 'p'
-  0x09, //113 'q'
-  0x09, //114 'r'
-  0x09, //115 's'
-  0x09, //116 't'
-  0x08, //117 'u'
-  0x08, //118 'v'
-  0x08, //119 'w'
-  0x09, //120 'x'
-  0x09, //121 'y'
-  0x09, //122 'z'
-  0x08, //123 '{'
-  0x08, //124 '|'
-  0x08, //125 '}'
-  0x08, //126 '~'
-  0x00  //127
-};
+static int Equivalent(enum CharType type, const char *character) {
+  static const char EQUIVALENT[] = {
+    0x00, //000 '\0'
+    0x00, //001
+    0x00, //002
+    0x00, //003
+    0x00, //004
+    0x00, //005
+    0x00, //006
+    0x00, //007
+    0x00, //008
+    0x18, //009 '\t'
+    0x04, //010 '\n'
+    0x00, //011
+    0x00, //012
+    0x04, //013 '\r'
+    0x00, //014
+    0x00, //015
+    0x00, //016
+    0x00, //017
+    0x00, //018
+    0x00, //019
+    0x00, //020
+    0x00, //021
+    0x00, //022
+    0x00, //023
+    0x00, //024
+    0x00, //025
+    0x00, //026
+    0x00, //027
+    0x00, //028
+    0x00, //029
+    0x00, //030
+    0x00, //031
+    0x18, //032 ' '
+    0x08, //033 '!'
+    0x00, //034 '"'
+    0x08, //035 '#'
+    0x08, //036 '$'
+    0x08, //037 '%'
+    0x08, //038 '&'
+    0x00, //039 '''
+    0x00, //040 '('
+    0x00, //041 ')'
+    0x09, //042 '*'
+    0x08, //043 '+'
+    0x08, //044 ','
+    0x08, //045 '-'
+    0x08, //046 '.'
+    0x08, //047 '/'
+    0x0A, //048 '0'
+    0x0A, //049 '1'
+    0x0A, //050 '2'
+    0x0A, //051 '3'
+    0x0A, //052 '4'
+    0x0A, //053 '5'
+    0x0A, //054 '6'
+    0x0A, //055 '7'
+    0x0A, //056 '8'
+    0x0A, //057 '9'
+    0x08, //058 ':'
+    0x08, //059 ';'
+    0x08, //060 '<'
+    0x08, //061 '='
+    0x08, //062 '>'
+    0x08, //063 '?'
+    0x08, //064 '@'
+    0x09, //065 'A'
+    0x09, //066 'B'
+    0x09, //067 'C'
+    0x09, //068 'D'
+    0x09, //069 'E'
+    0x09, //070 'F'
+    0x09, //071 'G'
+    0x09, //072 'H'
+    0x09, //073 'I'
+    0x09, //074 'J'
+    0x09, //075 'K'
+    0x09, //076 'L'
+    0x09, //077 'M'
+    0x09, //078 'N'
+    0x08, //079 'O'
+    0x09, //080 'P'
+    0x09, //081 'Q'
+    0x09, //082 'R'
+    0x09, //083 'S'
+    0x09, //084 'T'
+    0x08, //085 'U'
+    0x08, //086 'V'
+    0x08, //087 'W'
+    0x09, //088 'X'
+    0x09, //089 'Y'
+    0x09, //090 'Z'
+    0x08, //091 '['
+    0x08, //092 '\'
+    0x08, //093 ']'
+    0x08, //094 '^'
+    0x08, //095 '_'
+    0x08, //096 '`'
+    0x09, //097 'a'
+    0x09, //098 'b'
+    0x09, //099 'c'
+    0x09, //100 'd'
+    0x08, //101 'e'
+    0x09, //102 'f'
+    0x09, //103 'g'
+    0x09, //104 'h'
+    0x09, //105 'i'
+    0x09, //106 'j'
+    0x09, //107 'k'
+    0x09, //108 'l'
+    0x09, //109 'm'
+    0x09, //110 'n'
+    0x08, //111 'o'
+    0x09, //112 'p'
+    0x09, //113 'q'
+    0x09, //114 'r'
+    0x09, //115 's'
+    0x09, //116 't'
+    0x08, //117 'u'
+    0x08, //118 'v'
+    0x08, //119 'w'
+    0x09, //120 'x'
+    0x09, //121 'y'
+    0x09, //122 'z'
+    0x08, //123 '{'
+    0x08, //124 '|'
+    0x08, //125 '}'
+    0x08, //126 '~'
+    0x00  //127
+  };
+  if(*character >= 0 && *character <= 127)
+    return (EQUIVALENT[*character] & type) > 0;
+  return 0;
+}
 
 
-static const int MAX_POWER = 10;
-static const double TEN_TO_POWER_NEGATIVE[] = {
-  1.0,
-  0.1,
-  0.01,
-  0.001,
-  0.0001,
-  0.00001,
-  0.000001,
-  0.0000001,
-  0.00000001,
-  0.000000001,
-  0.0000000001
-};
-static const double TEN_TO_POWER_POSITIVE[] = {
-  1.0,
-  10.0,
-  100.0,
-  1000.0,
-  10000.0,
-  100000.0,
-  1000000.0,
-  10000000.0,
-  100000000.0,
-  1000000000.0,
-  10000000000.0
-};
+
 
 
 /*
@@ -204,12 +182,39 @@ static const char* Sign(double *value, const char *string) {
   string: Points to possible digits to parse.
 */
 static const char* Digits(double *value, int fractional, const char *string) {
+  static const int MAX_POWER = 10;
+  static const double TEN_TO_POWER_NEGATIVE[] = {
+    1.0,
+    0.1,
+    0.01,
+    0.001,
+    0.0001,
+    0.00001,
+    0.000001,
+    0.0000001,
+    0.00000001,
+    0.000000001,
+    0.0000000001
+  };
+  static const double TEN_TO_POWER_POSITIVE[] = {
+    1.0,
+    10.0,
+    100.0,
+    1000.0,
+    10000.0,
+    100000.0,
+    1000000.0,
+    10000000.0,
+    100000000.0,
+    1000000000.0,
+    10000000000.0
+  };
   //Starting at most signicant digit of whole or fractional number.
   *value = 0.0;
   int power = 0;
   //Parse digits to value. Limit places to look table size.
-  while(power < MAX_POWER && EQUIVALENT[*string] & DIGIT_CHAR)
-    *value += (*(string++) - '0') * TEN_TO_POWER_NEGATIVE[++power];
+  while(power < MAX_POWER && Equivalent(digitChar, string))
+    *value += (*string++ - '0') * TEN_TO_POWER_NEGATIVE[++power];
   //Digits represent a whole number, not a fractional number. Shift entire
   //value left of the decimal point.
   if(!fractional)
@@ -232,7 +237,7 @@ static const char* Number(double *value, const char *string) {
   const char *afterDecimal = *afterWhole == '.' ? afterWhole + 1 : afterWhole;
   const char *afterFraction = Digits(&fraction, 1, afterDecimal);
   //No digits parsed, no number.
-  if( !((afterWhole - afterSign) + (afterFraction - afterDecimal)) )
+  if(afterWhole == afterSign && afterFraction == afterDecimal)
     return string;
   //Calculate numbers value.
   *value = sign * (whole + fraction);
@@ -249,7 +254,7 @@ static const char* Number(double *value, const char *string) {
 */
 static const char* Argument(char *letter, double *value, const char *string) {
   //No argument.
-  if(!(EQUIVALENT[*string] & ARGUMENT_CHAR))
+  if(!Equivalent(argumentChar, string))
     return string;
   //Parse argument and optional number.
   *letter = *string;
@@ -257,55 +262,66 @@ static const char* Argument(char *letter, double *value, const char *string) {
 }
 
 
+
 /*
-  returns: Given string if not parsed or comment is to long for buffer.
+  returns: Given string if not parsed or text is to long for buffer.
+           Pointer to char after text comment if parsed.
+  buffer: Filled with null delimited text if (returned pointer > given
+          string pointer).
+  capacity: Size of the buffer in smallest addressable units of memory.
+  string: Points to possible text to parse.
+*/
+static const char* Text(char* buffer, int capacity, const char *string) {
+  //Parse Optional text.
+  const char *current = string;
+  int size = 0;
+  while((size += sizeof(char)) < capacity && Equivalent(textChar, current))
+    *buffer++ = *current++;
+  //Text is to long for buffer.
+  if(Equivalent(textChar, current))
+    return string;
+  //Successfully pasred text.
+  buffer = '\0';
+  return current;
+}
+
+
+static char comment[257];
+static int COMMENT_CAPACITY = 257;
+static char commentAvailable = 0;
+
+
+/*
+  returns: Given string if not parsed.
            Pointer to char after inline comment if parsed.
-  buffer: Filled with null delimited comment text if (returned pointer >
-          given string pointer).
-  capacity: Size of the buffer.
   string: Points to possible inline comment to parse.
 */
-static const char* InlineComment(char* buffer, int capacity, const char *string) {
+static const char* InlineComment(const char *string) {
   //No opening parenthesis.
   if(*string != '(')
     return string;
-  //Parse Optional text.
-  int index = 0;
-  const char *current = string + 1;
-  while(index < capacity - 1 && EQUIVALENT[*current] & TEXT_CHAR)
-    buffer[index++] = *(current++);
+  const char *afterText = Text(comment, COMMENT_CAPACITY, string + 1);
   //Text is to long or no closing parenthesis.
-  if(*current != ')')
+  if(*afterText != ')')
     return string;
-  //Successfully pasred comment.
-  buffer[index] = '\0';
-  return ++current;
+  return ++afterText;
 }
 
 
 /*
-  returns: Given string if not parsed or comment is to long for buffer.
+  returns: Given string if not parsed.
            Pointer to char after ending comment if parsed.
-  buffer: Filled with null delimited comment string if (returned pointer >
-          given string pointer).
-  capacity: Size of the buffer.
   string: Points to possible ending comment to parse.
 */
-static const char* EndingComment(char* buffer, int capacity, const char *string) {
+static const char* EndingComment(const char *string) {
   //No starting semicolon.
   if(*string != ';')
     return string;
-  //Parse Optional text.
-  int index = 0;
-  const char *current = string + 1;
-  while(index < capacity - 1 && EQUIVALENT[*current] & TEXT_CHAR)
-    buffer[index++] = *(current++);
+  const char *afterText = Text(comment, COMMENT_CAPACITY, string + 1);
   //Text is to long or no newline.
-  if(!(EQUIVALENT[*current] & NEWLINE_CHAR))
+  if(!Equivalent(newlineChar, afterText))
     return string;
-  //Successfully pasred comment.
-  buffer[index] = '\0';
-  return current;
+  return afterText;
 }
 
 
@@ -315,7 +331,7 @@ static const char* EndingComment(char* buffer, int capacity, const char *string)
   string: Points to possible newline to parse.
 */
 static const char* Newline(const char *string) {
-  while(EQUIVALENT[*string] & NEWLINE_CHAR)
+  while(Equivalent(newlineChar, string))
     string++;
   return string;
 }
@@ -327,7 +343,7 @@ static const char* Newline(const char *string) {
   string: Points to possible whitespace to parse.
 */
 static const char* Whitespace(const char *string) {
-  while(EQUIVALENT[*string] & WHITESPACE_CHAR)
+  while(Equivalent(whitespaceChar, string))
     string++;
   return string;
 }
@@ -335,7 +351,7 @@ static const char* Whitespace(const char *string) {
 
 /*
   returns: Given string if not parsed or to many arguments for buffer.
-           Pointer to char after contents if parsed.
+           Pointer to char after line if parsed.
   buffer: Filled with arguments in binary format if (returned pointer >
           given string pointer).
             Binary Format Example:
@@ -350,21 +366,85 @@ static const char* Whitespace(const char *string) {
                                        Argument letter 'X'    |     |
                                    Argument number present FALSE    |
                                                 No more arguments '\0'
-  capacity: Size of the buffer.
-  string: Points to possible ending comment to parse.
+  capacity: Size of the buffer in smallest addressable units of memory.
+  string: Points to possible line to parse.
 */
-static const char* Contents(char* buffer, int capacity, const char *string)
-
-
+static const char* Line(void* buffer, int capacity, const char *string) {
+  const char *before, *afterArgument, *afterWhitespace, *afterEndingComment,
+    *afterNewline, *afterInlineComment = string;
+  char letter;
+  int size = 0;
+  double value;
+  do {
+    before = afterInlineComment;
+    afterArgument = Argument(&letter, &value, before);
+    //Optional Argument parsed, put it in the buffer in binary format.
+    if(afterArgument > before) {
+      //Buffer is to small to hold addtional letter and value flag.
+      if((size += sizeof(char) * 2) > capacity - sizeof(char))
+        return string;
+      *((char*) buffer) = letter;
+      buffer = ((char*) buffer) + 1;
+      //Arguemnt has a value.
+      if(afterArgument > before + 1) {
+        //Buffer is to small to hold addtional value.
+        if((size += sizeof(double)) > capacity - sizeof(char))
+          return string;
+        *((char*) buffer) = 1;
+        buffer = ((char*) buffer) + 1;
+        *((double*) buffer) = value;
+        buffer = ((double*) buffer) + 1;
+      }
+      //Argument has no value.
+      else {
+        *((char*) buffer) = 0;
+        buffer = ((char*) buffer) + 1;
+      }
+    }
+    afterWhitespace = Whitespace(afterArgument);
+    afterInlineComment = InlineComment(afterWhitespace);
+    //Optional comment parsed.
+    if(afterInlineComment > afterWhitespace)
+      commentAvailable = 1;
+  } while(afterInlineComment > before);
+  //Successfully pasred arguments to binary format.
+  *((char*) buffer) = '\0';
+  afterEndingComment = EndingComment(before);
+  //Optional comment parsed.
+  if(afterEndingComment > before)
+    commentAvailable = 1;
+  afterNewline = Newline(afterEndingComment);
+  //No newline.
+  if(afterNewline == afterEndingComment)
+    return string;
+  return afterNewline;
+}
 
 
 int main() {
-  const char *command = "m42 ;The best command \n\r";
-  char buffer[18];
+  const char *command = "g0 X (X is a flag) Y Z (this is the last comment)"
+                        " E-0.1 ;The best command \n\r\0";
+  char buffer[27];
+  const char *after = Line(buffer, 27, (command));
+  if(after > (command)) {
 
-  const char *after = EndingComment(buffer, 18, (command + 4));
-  if(after > (command + 4))
-    printf("[%s]", buffer);
+    //Display any args found.
+    int index = 0;
+    while(buffer[index] != '\0') {
+      printf("[%c", buffer[index++]);
+      if(buffer[index++] == 1) {
+        printf("%lf", *((double*)(buffer + index)));
+        index += sizeof(double);
+      }
+      printf("]");
+    }
+
+    //Display last inline comment found.
+    if(commentAvailable){
+      commentAvailable = 0;
+      printf("[%s]", comment);
+    }
+  }
   printf("%s", after);
   return 0;
 }
